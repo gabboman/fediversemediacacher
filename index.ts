d50608cbca932d7e86a4681c4c7b9e6c74b9b21c
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-01-20 19:26:47 +0100

    updated user agent

--- index.ts
+++ index.ts
@@ -1,64 +1,70 @@
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors'
-import axios from 'axios';
-const fs = require('fs')
-const { http, https } = require('follow-redirects');
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import axios from "axios";
+const fs = require("fs");
+const { http, https } = require("follow-redirects");
 
-const environment = require('./environment')
-const crypto = require('crypto');
+const environment = require("./environment");
+const crypto = require("crypto");
 
-const app = express()
-const PORT = environment.port
-const validExtensions = environment.validFormats
+const app = express();
+const PORT = environment.port;
+const validExtensions = environment.validFormats;
 
-app.use(cors())
+app.use(cors());
 
-
-app.get('/', async (req: Request, res: Response) =>{
-        if(req.query?.media) {
-            try {
-                const mediaLink: string = req.query.media as string
-                const mediaLinkArray = mediaLink.split('.')
-                let linkExtension = mediaLinkArray[mediaLinkArray.length -1].toLowerCase()
-                // calckey images have no extension
-                if(validExtensions.indexOf(linkExtension) === -1 ){
-                    linkExtension = ''
-                }
-                const mediaLinkHash = crypto.createHash('sha256').update(mediaLink).digest('hex')
-                const localFileName = linkExtension ? `cache/${mediaLinkHash}.${linkExtension}`: `cache/${mediaLinkHash}`
-                if (fs.existsSync(localFileName)) {
-                    // We have the image! we just serve it
-                    res.set('Cache-control', 'public, max-age=36000')
-                    res.sendFile(localFileName, { root: '.' })
-                  } else {
-                    // its downloading time!
-                    try {
-                        const remoteResponse = await axios.get(mediaLink, {responseType: 'stream'})
-                        const path = `${__dirname}/${localFileName}`;
-                        const filePath = fs.createWriteStream(path);
-                        filePath.on('finish',() => {
-                            filePath.close();
-                            res.set('Cache-control', 'public, max-age=36000')
-                            res.sendFile(localFileName, { root: '.' })
-                            })
-                        remoteResponse.data.pipe(filePath);
-                    } catch (error) {
-                        console.log(error)
-                        res.sendStatus(404)
-                    }
-                  }
-
-            }catch (error) {
-                res.sendStatus(500);
-                res.send();
-                console.log(error)
-            }
-        } else {
-            res.sendStatus(404)
+app.get("/", async (req: Request, res: Response) => {
+  if (req.query?.media) {
+    try {
+      const mediaLink: string = req.query.media as string;
+      const mediaLinkArray = mediaLink.split(".");
+      let linkExtension =
+        mediaLinkArray[mediaLinkArray.length - 1].toLowerCase();
+      // calckey images have no extension
+      if (validExtensions.indexOf(linkExtension) === -1) {
+        linkExtension = "";
+      }
+      const mediaLinkHash = crypto
+        .createHash("sha256")
+        .update(mediaLink)
+        .digest("hex");
+      const localFileName = linkExtension
+        ? `cache/${mediaLinkHash}.${linkExtension}`
+        : `cache/${mediaLinkHash}`;
+      if (fs.existsSync(localFileName)) {
+        // We have the image! we just serve it
+        res.set("Cache-control", "public, max-age=36000");
+        res.sendFile(localFileName, { root: "." });
+      } else {
+        // its downloading time!
+        try {
+          const remoteResponse = await axios.get(mediaLink, {
+            responseType: "stream",
+            headers: { "User-Agent": "wafrn-cache-generator" },
+          });
+          const path = `${__dirname}/${localFileName}`;
+          const filePath = fs.createWriteStream(path);
+          filePath.on("finish", () => {
+            filePath.close();
+            res.set("Cache-control", "public, max-age=36000");
+            res.sendFile(localFileName, { root: "." });
+          });
+          remoteResponse.data.pipe(filePath);
+        } catch (error) {
+          console.log(error);
+          res.sendStatus(404);
         }
+      }
+    } catch (error) {
+      res.sendStatus(500);
+      res.send();
+      console.log(error);
     }
-)
+  } else {
+    res.sendStatus(404);
+  }
+});
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`⚡️Server is running at https://localhost:${PORT}`)
-  })
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`⚡️Server is running at https://localhost:${PORT}`);
+});
